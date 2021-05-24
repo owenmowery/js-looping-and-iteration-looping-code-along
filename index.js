51b2781e280f9d4d32a4a39bd2df1147c94b8908
Owen Mowery <owenmowery99@gmail.com>
2021-05-24 14:56:44 -0400

    commit tree f4f6707f5125702fe87daeca060b03d80bb1f295

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    let messages = [];
+    for (let i = 0; i <= names.length - 1; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+
+    return messages;
+}
+
+function countDown(num) {
+    for (let i = num; i >= 0; i--) {
+        console.log(i);
+    }
+}
